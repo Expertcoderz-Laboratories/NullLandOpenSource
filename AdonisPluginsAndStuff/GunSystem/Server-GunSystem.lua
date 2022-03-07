2a943ad4d3a656901d7c00c681aba9b730b07e09
Expertcoderz <81153405+Expertcoderz@users.noreply.github.com>
2022-03-07 22:27:36 +0800

    Update Server-GunSystem.lua

--- AdonisPluginsAndStuff/GunSystem/Server-GunSystem.lua
+++ AdonisPluginsAndStuff/GunSystem/Server-GunSystem.lua
@@ -3,15 +3,20 @@
 	Description: The serverside component of the Adonis Gun System.
 	Author: Expertcoderz
 	Release Date: 2022-02-11 (project started in December 2021; originated from Aug/Sep 2021)
-
-	THIS REQUIRES THE CollectionServiceHandler PLUGIN TO FUNCTION.
+	Last Updated: 2022-03-07
 --]]
 
 server, service = nil, nil
 
 return function()
+	local Players: Players = service.Players
+
 	local function Debug(...)
 		warn("[GunServer]", ...)
+	end
+
+	local function ExploitDetected(plr: Player, action: string, desc: string, ...: any)
+		return server.Anti.Detected(plr, action, "[Gun System] "..string.format(desc, ...))
 	end
 
 	local ReplicatedGunConfigs = service.New("Folder", {
@@ -79,6 +84,9 @@ return function()
 			if not config._AmmoBillboard and module:FindFirstChild("AmmoBillboard") then
 				config._AmmoBillboard = module.AmmoBillboard
 			end
+			if config._Modifier then
+				task.defer(config._Modifier, config, service, server)
+			end
 			if module.Name == "_Base" then
 				return config
 			end
@@ -130,7 +138,7 @@ return function()
 			local self = {Root = tool;}
 			setmetatable(self, Gun)
 
-			if tool.Parent and not tool.Parent:IsA("Backpack") and not service.Players:GetPlayerFromCharacter(tool.Parent) then
+			if tool.Parent and not tool.Parent:IsA("Backpack") and not Players:GetPlayerFromCharacter(tool.Parent) then
 				return nil
 			end
 
@@ -142,16 +150,19 @@ return function()
 
 				self.handles = {}
 				for _, v in ipairs(tool:GetChildren()) do
-					if v.Name:sub(1, 6) == "Handle" then
+					if v.Name == "Handle" or (v.Name:sub(1, 6) == "Handle" and tonumber(v.Name:sub(7, #v.Name))) then
 						table.insert(self.handles, v)
 					end
 				end
 
 				self.ConfigModule = LoadConfig(tool.Name) :: ModuleScript
 				self.config = configCaches[tool.Name] :: {[string]:any}
-				if not self.config then
+				local config = self.config
+
+				if not config then
 					configCaches[tool.Name] = GetFullConfig(tool.Name)
-					self.config = configCaches[tool.Name]
+					config = configCaches[tool.Name]
+					self.config = config
 				end
 
 				self.CurrentHumanoid = nil :: Humanoid?
@@ -202,26 +213,26 @@ return function()
 
 				--// Create gun animation objects
 				for _, v in ipairs({"Idle", "Fire", "Reload", "ShotgunClipin", "HoldDown", "Equip", "Aiming"}) do
-					if self.config[v.."AnimationId"] ~= 0 then
+					if config[v.."AnimationId"] ~= 0 then
 						tagTemp(service.New("Animation", {
 							Parent = tool;
 							Name = v.."Anim";
-							AnimationId = "rbxassetid://"..self.config[v.."AnimationId"];
+							AnimationId = "rbxassetid://"..config[v.."AnimationId"];
 						}))
 					end
 				end
 
 				if not tool:GetAttribute("CurrentMag") then
-					tool:SetAttribute("CurrentMag", self.config.AmmoPerMag)
-					tool:SetAttribute("CurrentAmmo", self.config.LimitedAmmo and self.config.Ammo or 0)
+					tool:SetAttribute("CurrentMag", config.AmmoPerMag)
+					tool:SetAttribute("CurrentAmmo", config.LimitedAmmo and config.Ammo or 0)
 				end
 
-				self.AmmoGui = self.handles[1]:FindFirstChild("AmmoBillboard") or self.config._AmmoBillboard:Clone()
+				self.AmmoGui = self.handles[1]:FindFirstChild("AmmoBillboard") or config._AmmoBillboard:Clone()
 				self.AmmoGui.Parent = self.handles[1]
-				self.AmmoGui.Enabled = self.config.ShowAmmoBillboard
+				self.AmmoGui.Enabled = config.ShowAmmoBillboard
 				self.AmmoGuiLabel = self.AmmoGui:FindFirstChild("AmmoLabel", true)
 				local function updateAmmoGui()
-					self.AmmoGuiLabel.Text = string.format("Ammo: %s/%s", server.Functions.FormatNumber(tool:GetAttribute("CurrentMag")), if self.config.LimitedAmmo then server.Functions.FormatNumber(tool:GetAttribute("CurrentAmmo")) else "Inf")
+					self.AmmoGuiLabel.Text = string.format("Ammo: %s/%s", server.Functions.FormatNumber(tool:GetAttribute("CurrentMag")), if config.LimitedAmmo then server.Functions.FormatNumber(tool:GetAttribute("CurrentAmmo")) else "Inf")
 					if self.AmmoGuiLabel.Text == "Ammo: Inf/Inf" then
 						self.AmmoGuiLabel.Text = "Ammo: Inf"
 					end
@@ -229,16 +240,16 @@ return function()
 				updateAmmoGui()
 
 				table.insert(self.connections, tool.Equipped:Connect(function()
-					local speedReduction = self.config.WalkspeedReduction
+					local speedReduction = config.WalkspeedReduction
 					task.wait(0.1)
 					if speedReduction ~= 0 and self.CurrentHumanoid then
 						self.CurrentHumanoid:SetAttribute("_OriginalWalkspeed", self.CurrentHumanoid.WalkSpeed)
-						self.CurrentHumanoid.WalkSpeed -= self.config.WalkspeedReduction
+						self.CurrentHumanoid.WalkSpeed -= config.WalkspeedReduction
 					end
 				end))
 
 				table.insert(self.connections, tool.Unequipped:Connect(function()
-					if self.config.WalkspeedReduction ~= 0 and self.CurrentHumanoid then
+					if config.WalkspeedReduction ~= 0 and self.CurrentHumanoid then
 						self.CurrentHumanoid.WalkSpeed = self.CurrentHumanoid:GetAttribute("_OriginalWalkspeed") or 16
 					end
 				end))
@@ -260,11 +271,11 @@ return function()
 					end,
 					VisualizeBulletSet = function(plr: Player, fireDirs: {Vector3}, handle: BasePart)
 						if not type(fireDirs) == "table" and typeof(handle) == "Instance" then
-							server.Anti.Detected(plr, "kill", "[Gun System] Invalid remote data for VisualizeBulletSet")
+							ExploitDetected(plr, "kill", "Invalid remote data for VisualizeBulletSet (fireDirs: %s, handle: %s)", tostring(fireDirs), tostring(handle))
 							return
 						end
-						if #fireDirs ~= self.config.BulletPerShot and #fireDirs ~= 1 then
-							server.Anti.Detected(plr, "kick", "[Gun System] Attempt to fire bullet set of incorrect size")
+						if #fireDirs ~= config.BulletsPerShot and #fireDirs ~= 1 then
+							ExploitDetected(plr, "kick", "Attempt to fire bullet set of incorrect size (expected %d, got %d)", config.BulletsPerShot, #fireDirs)
 							return
 						end
 						for i, handle in pairs(self.handles) do
@@ -274,33 +285,44 @@ return function()
 							end
 						end
 						self.RemotesFolder.VisualizeBulletSet:FireAllClients(plr, fireDirs, handle)
-						server.Functions.AddToGlobalStat("Shots Fired", #fireDirs)
+						if server.Functions.AddToGlobalStat then
+							server.Functions.AddToGlobalStat("Shots Fired", #fireDirs)
+						end
 					end,
 					VisualizeMuzzle = function(plr: Player, ...)
 						self.RemotesFolder.VisualizeMuzzle:FireAllClients(plr, ...)
 					end,
 					InflictTarget = function(plr: Player, targetHitPart: BasePart)
-						local targetHum = (targetHitPart and targetHitPart.Parent) and targetHitPart.Parent:FindFirstChildOfClass("Humanoid")
-						local targetPlr = targetHum and service.Players:GetPlayerFromCharacter(targetHitPart.Parent)
+						local targetChar = targetHitPart.Parent
+						local targetHum = targetChar and targetChar:FindFirstChildOfClass("Humanoid")
+						local targetPlr = targetHum and Players:GetPlayerFromCharacter(targetChar)
+
 						if targetHum and targetHum.Health ~= 0 and (not targetPlr or not workspace:GetAttribute("_TeamkillDisabled") or not targetPlr.Team or plr.Team ~= targetPlr.Team) then
-							server.Functions.ApplyDeathTag(targetHum, plr)
-							local damageAmount = self.config.BaseDamage * if targetHitPart.Name == "Head" then self.config.HeadshotDamageMultiplier else 1
-							targetHum:TakeDamage(damageAmount)
-							if self.config.Knockback > 0 and plr ~= targetPlr then
-								local shover = plr.Character.PrimaryPart or plr.Character.Head
-								local duration = 0.1
-								local shoveForce = service.New("BodyVelocity", {
-									MaxForce = Vector3.new(1e9, 1e9, 1e9);
-									Velocity = (targetHitPart.Position - shover.Position).unit * (self.config.Knockback / duration);
-								})
-								shoveForce.Parent = targetHitPart
-								service.Debris:AddItem(shoveForce, duration)
+							server.Functions.ApplyDeathTag(targetHum, plr.DisplayName.." using "..tool.Name)
+
+							local damageAmount = config.BaseDamage * if targetHitPart.Name == "Head" then config.HeadshotDamageMultiplier else 1
+							if config.DisregardProtection then
+								targetHum.Health -= damageAmount
+							else
+								targetHum:TakeDamage(damageAmount)
 							end
-							for _, effect in ipairs(self.config.SpecialEffects) do
-								local context = {server = server, service = service, Attacker = plr, AttackerChar = plr.Character, Damage = damageAmount, Weapon = tool}
-								table.foreach(effect, function(key, val)
-									context[key] = val
-								end)
+
+							if config.Knockback > 0 and plr ~= targetPlr then
+								local shover: BasePart = plr.Character and plr.Character.PrimaryPart
+								if shover then
+									local duration = 0.1
+									service.Debris:AddItem(service.New("BodyVelocity", {
+										Parent = targetHitPart;
+										MaxForce = Vector3.new(1e9, 1e9, 1e9);
+										Velocity = (targetHitPart.Position - shover.Position).Unit * (config.Knockback / duration);
+									}), duration)
+								end
+							end
+
+							for _, effect in ipairs(config.SpecialEffects) do
+								local context = {server = server, service = service, Attacker = plr, AttackerChar = plr.Character, Damage = damageAmount, Weapon = tool, SpecialEffects = specialEffects}
+								table.foreach(effect, function(key, val) context[key] = val end)
+
 								xpcall(if type(effect) == "table" then specialEffects[effect.Effect] else effect, function(err)
 									Debug("SpecialEffect application error:", err)
 								end, context, targetHum.Parent)
@@ -308,14 +330,14 @@ return function()
 						end
 					end,
 					ChangeMagAndAmmo = function(plr: Player, mag: number, ammo: number, reloading: boolean?)
-						if not tonumber(mag) or not tonumber(ammo) or ammo > tool:GetAttribute("CurrentAmmo") or mag > self.config.AmmoPerMag then
-							server.Anti.Detected(plr, "kick", "[Gun System] Attempt to set mag/ammo to an illegal value")
+						if not tonumber(mag) or not tonumber(ammo) or ammo > tool:GetAttribute("CurrentAmmo") or mag > config.AmmoPerMag then
+							ExploitDetected(plr, "kick", "Attempt to set mag/ammo to an illegal value (mag: %s, ammo: %s, CurrentAmmo: %d, AmmoPerMag: %d)", tostring(mag), tostring(ammo), tool:GetAttribute("CurrentAmmo"), config.AmmoPerMag)
 							return
 						end
 						tool:SetAttribute("CurrentMag", mag)
 						tool:SetAttribute("CurrentAmmo", ammo)
 						updateAmmoGui()
-						if self.config.RocketLauncher then
+						if config.RocketLauncher then
 							for _, v in pairs(tool.Rocket:GetChildren()) do
 								if v:IsA("BasePart") then
 									v.Transparency = if reloading then 0 else 1
@@ -330,21 +352,30 @@ return function()
 						Name = name;
 					}, nil, true)
 					table.insert(self.connections, event.OnServerEvent:Connect(function(plr: Player, ...)
-						if plr == service.Players:GetPlayerFromCharacter(tool.Parent) or plr == tool.Parent.Parent then
-							callback(plr, ...)
+						local gunHolder = Players:GetPlayerFromCharacter(tool.Parent) or tool.Parent.Parent
+						if plr == gunHolder then
+							if plr.Character and plr.Character.PrimaryPart then
+								callback(plr, ...)
+							else
+								Debug(string.format("%s fired %s for %s whilst without character/HRP", plr.Name, name, tool.Name))
+							end
 						else
-							server.Anti.Detected(plr, "kick", "[Gun System] Fired remote for someone else's gun")
+							ExploitDetected(plr, "kick", "Fired remote for someone else's gun (holder: %s)", if gunHolder and gunHolder:IsA("Player") then "@"..gunHolder.Name else "N/A")
 						end
 					end))
 				end
 				self.RemotesFolder.Parent = tool
 
-				for _, v in pairs(service.Players:GetPlayers()) do
+				for _, v in pairs(Players:GetPlayers()) do
 					server.Remote.Send(v, "RegisterGun", tool)
 				end
-				table.insert(self.connections, service.Players.PlayerAdded:Connect(function(plr)
+				table.insert(self.connections, Players.PlayerAdded:Connect(function(plr)
 					server.Remote.Send(plr, "RegisterGun", tool)
 				end))
+
+				if config._Execute then
+					config._Execute(tool, service, server)
+				end
 
 				return self
 			end, function(err)
@@ -371,7 +402,7 @@ return function()
 		--// Adonis Gun pkg object methods
 		function Gun:setAmmo(amount: number)
 			self.Root:SetAttribute("CurrentAmmo", amount)
-			self.RemotesFolder.ChangeMagAndAmmo:FireClient(service.Players:GetPlayerFromCharacter(self.Root.Parent) or self.Root.Parent.Parent)
+			self.RemotesFolder.ChangeMagAndAmmo:FireClient(Players:GetPlayerFromCharacter(self.Root.Parent) or self.Root.Parent.Parent)
 		end
 		function Gun:refillAmmo()
 			self:setAmmo(self.config.MaxAmmo)
@@ -418,16 +449,16 @@ return function()
 			Function = function(plr: Player, args: {string})
 				local gun = plr.Character and plr.Character:FindFirstChildOfClass("Tool")
 				assert(gun, "Gun not equipped/character missing")
+
 				assert(not args[1] or args[1] == "Medium stone grey" or BrickColor.new(args[1]).Name ~= "Medium stone grey", "Invalid BrickColor supplied (refer to !colors for a list).")
+
 				local pkg = gun and server.Functions.GetCollectionServicePackageFromInstance(gun, "ADONIS_GUN")
-				if pkg then
-					if args[1] then
-						pkg:setCamo(BrickColor.new(args[1]))
-					else
-						pkg:resetCamo()
-					end
+				assert(pkg, "A valid gun must be equipped")
+
+				if args[1] then
+					pkg:setCamo(BrickColor.new(args[1]))
 				else
-					error("A valid gun must be equipped")
+					pkg:resetCamo()
 				end
 			end
 		}
@@ -442,7 +473,7 @@ return function()
 				workspace:SetAttribute("_TeamkillDisabled", if args[1] and args[1]:lower() == "on" then false
 					elseif args[1] and args[1]:lower() == "off" then true
 					else not workspace:GetAttribute("_TeamkillDisabled"))
-				server.Functions.Hint(string.format("Teamkill is now %s.", workspace:GetAttribute("_TeamkillDisabled") and "disabled" or "enabled"), service.Players:GetPlayers())
+				server.Functions.Hint(string.format("Teamkill is now %s.", workspace:GetAttribute("_TeamkillDisabled") and "disabled" or "enabled"), Players:GetPlayers())
 			end
 		}
 
@@ -455,28 +486,26 @@ return function()
 			Function = function(plr: Player, args: {string})
 				local gun = plr.Character and plr.Character:FindFirstChildOfClass("Tool")
 				assert(gun, "Gun not equipped/character missing")
+
 				local pkg = gun and server.Functions.GetCollectionServicePackageFromInstance(gun, "ADONIS_GUN")
-				if pkg then
-					local tab = {}
-					for setting: string, value: any in pairs(pkg.config) do
-						if type(setting) ~= "string" or setting:sub(1, 1) == "_" then continue end
-						table.insert(tab, setting..": "..if type(value) == "table" then service.HttpService:JSONEncode(value) else tostring(value))
-					end
-					table.sort(tab)
-					server.Remote.MakeGui(plr, "List", {
-						Title = gun.Name.." Stats";
-						Icon = server.MatIcons.Leaderboard;
-						Tab = tab;
-						Size = {290, 350};
-					})
-				else
-					error("A valid gun must be equipped")
+				assert(pkg, "A valid gun must be equipped")
+
+				local tab = {}
+				for setting: string, value: any in pairs(pkg.config) do
+					if type(setting) ~= "string" or setting:sub(1, 1) == "_" then continue end
+					table.insert(tab, setting..": "..if type(value) == "table" then service.HttpService:JSONEncode(value) else tostring(value))
 				end
+				table.sort(tab)
+				server.Remote.MakeGui(plr, "List", {
+					Title = gun.Name.." Stats";
+					Icon = server.MatIcons.Leaderboard;
+					Tab = tab;
+					Size = {290, 350};
+				})
 			end
 		}
 
 		Debug("Installation complete")
-		if _G._ESIAS then _G._RaiseFlag("GunSystem") end
 	end, function(err)
 		Debug("Installation error:", err)
 	end)
